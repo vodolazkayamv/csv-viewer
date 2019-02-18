179d89ff437928beacfa7b494713bb59e72bc325
Мария Водолазкая <vodolazkayamv@gmail.com>
2019-02-18 15:39:42 +0300

    my csv code

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const csv=require('csvtojson')
+ 
+var app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+ 
+app.get('/notes', function(req, res) {
+  res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+})
+
+app.get('/csv', function(req, res) {
+	csv()
+		.fromFile(csvFilePath)
+		.then((jsonObj)=>{
+		    res.json(jsonObj)
+		})
+})
+ 
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const csvFilePath='myfile.csv'
+
